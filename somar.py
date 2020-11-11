336527a3919a44e822e23b6bd93da730f69547c8
Andre Santos Barros da Silva <andrezio@live.com>
2020-11-11 20:51:50 -0300

    Update somar.py

--- somar.py
+++ somar.py
@@ -1,2 +1,26 @@
 def adicao(num1, num2):
     return num1 + num2
+
+def subtracao(num1, num2):
+    return num1 - num2
+
+def delta(a, b, c):
+    delta = b*2 - 4*a*c
+    return delta
+
+def check_delta(delta):
+    if delta >= 0:
+        return True
+    else:
+        return False
+
+def calc_raizes(a, b, c):
+    # TODO: Calcular raizes
+    pass
+    
+def baskara(a, b, c):
+    delt = delta(a, b, c)
+    if check_deta(delt):
+        return calc_raizes(a, b, c)
+    else:
+        return False
